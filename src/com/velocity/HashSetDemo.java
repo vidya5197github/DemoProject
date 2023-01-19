54f8ca0f1866c1771270fff81efa914aea5559f3
vidya <vidyamore2222@gmail.com>
2023-01-19 10:55:59 +0530

    Added Data

---
+++ src/com/velocity/HashSetDemo.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+    HashSet hs=new HashSet();
+    hs.add(10);
+    hs.add(20);
+    hs.add(30);
+      System.out.println(hs);
+	}
+	
+}
