5041c5563527644b4507c431e4eb988a01a91b3c
vidya <vidyamore2222@gmail.com>
2023-01-19 11:04:16 +0530

    New Class Added

---
+++ src/com/velocity/LinkedHashSetDemo.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDemo {
+
+	public static void main(String[] args) {
+    LinkedHashSet<String> set=new LinkedHashSet<String>();
+ set.add("Java");
+ set.add("SpringBoot");
+ set.add("Sting");
+
+System.out.println(set);
+	}
+
+}
